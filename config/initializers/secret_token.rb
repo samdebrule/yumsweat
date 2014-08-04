b091ecd33418cefbaf9de81574af49aaae664029
Sam DeBrule <sam.debrule@gmail.com>
2014-08-04 18:54:08 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yumsweat::Application.config.secret_key_base = 'd954f8c5ba9b688a4694014c6c988661894dbf4cc766cd8469e2660c9e5cff67436f093899db8059b29a2c99e6bfa382dd590d0fec6be64378603c71c7189e6c'
